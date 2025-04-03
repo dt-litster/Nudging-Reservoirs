3d1aae90ea254af2a2c95464257eb045cd648b2f
David Thomas Litster <90051576+dt-litster@users.noreply.github.com>
2025-04-02 21:02:02 -0600

    Added visualization to utils

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from matplotlib import pyplot as plt
 
 def vpt_time(ts, Uts, pre, vpt_tol=5.):
     """
@@ -22,3 +22,41 @@ def vpt_time(ts, Uts, pre, vpt_tol=5.):
     else:
         vptime = ts[idx-1] - ts[0]
     return vptime
+
+
+def visualize(true_y, pred_y, t, title=None, savefig=False):
+    fig = plt.figure(figsize=(12, 4))
+    ax_traj = fig.add_subplot(121)
+    ax_phase = fig.add_subplot(122, projection="3d")
+
+    ax_traj.cla()
+    ax_traj.set_title('Trajectories')
+    ax_traj.set_xlabel('$t$')
+    ax_traj.set_ylabel('$u$')
+    ax_traj.plot(t, true_y, '-', label="True")
+    ax_traj.plot(t, pred_y, '--', label="Pred")
+    ax_traj.set_xlim(t.min(), t.max())
+    ax_traj.set_ylim(-25, 50)
+    ax_traj.legend()
+    plt.legend()
+
+    ax_phase.cla()
+    ax_phase.set_title('Phase Portrait')
+    ax_phase.set_xlabel('$u_1$')
+    ax_phase.set_ylabel('$u_2$')
+    ax_phase.set_zlabel('$u_3$')
+    ax_phase.plot(*true_y.T, 'g-', label="True")
+    ax_phase.plot(*pred_y.T, 'b--', label="Pred")
+    ax_phase.set_xlim(-25, 25)
+    ax_phase.set_ylim(-25, 25)
+    ax_phase.set_zlim(0, 50)
+    plt.legend()
+
+    fig.tight_layout()
+    fig.suptitle(title)
+    if savefig:
+        plt.savefig(title + '.png')
+    else:
+        plt.show()
+
+    plt.close()
